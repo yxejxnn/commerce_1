04ccc546f97d5db7dec9efed3b41053264addd0d
정예진 <yxejxnn@gmail.com>
2026-03-29 11:44:31 +0900

    Spet.3 Category 클래스 상품 리스트 필드와 생성자 작성

---
+++ src/Category.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Category {
+
+    // 속성
+    String categoryName; // 카테고리 이름
+    List<Product> productList; // 해당 카테고리에 속한 상품 목록
+
+    // 생성자
+    public Category(String categoryName, List<Product> productList) {
+        this.categoryName = categoryName;
+        this.productList = productList;
+    }
+
+    // 기능
+    // 카테고리 이름 반환
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+}
