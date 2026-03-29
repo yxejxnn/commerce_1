c361ce38fb80445dca528981dba771175be906f5
정예진 <yxejxnn@gmail.com>
2026-03-29 10:49:57 +0900

    Spet.1 Product 클래스 속성 및 생성자 작성

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,16 @@
 public class Product {
+
+    // 속성
+    String productName; // 상품명
+    int productPrice; // 상품 가격
+    String productDescription; // 상품 설명
+    int productStock; // 상품 재고수량
+
+    // 생성자
+    public Product(String productName, int productPrice, String productDescription, int productStock) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productDescription = productDescription;
+        this.productStock = productStock;
+    }
 }
