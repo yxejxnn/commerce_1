2bc3a4c26be74969fbeaab15a413b474df579415
정예진 <yxejxnn@gmail.com>
2026-03-29 11:00:09 +0900

    Spet.1 Main 상품 목록 출력

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         // 상품 생성
         Product galaxyS25 = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 30);
@@ -18,5 +21,17 @@ public class Main {
         productList.add(iphone16);
         productList.add(macBookPro);
         productList.add(airPodsPro);
+
+        // 프로그램 실행 시 상품 목록 출력
+        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+
+        // 향상된 for문으로 List 안에 상품(객체)을 하나씩 꺼내서 출력
+        // 향상된 for문은 인덱스 번호가 없기 때문에 번호를 저장할 변수를 따로 만듦
+        int number = 1;
+
+        for (Product product : productList) {
+            System.out.println(number + ". " + product.productName + " | " + String.format("%,d" , product.productPrice) + "원 | " + product.productDescription);
+            number++;
+        }
     }
 }
