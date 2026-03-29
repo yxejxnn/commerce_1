39c4d8aaea83bcc82e007729da8596475c236f1a
정예진 <yxejxnn@gmail.com>
2026-03-29 11:28:23 +0900

    Spet.2 CommerceSystem 클래스 상품 리스트 필드와 생성자 추가

---
+++ src/CommerceSystem.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class CommerceSystem {
+
+    // 속성
+    List<Product> productList; // 상품 목록
+
+    // 생성자
+    public CommerceSystem(List<Product> productList) {
+        this.productList = productList;
+    }
+}
