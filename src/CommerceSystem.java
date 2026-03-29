ae83610906b252776764b0dea33695f8423221f8
정예진 <yxejxnn@gmail.com>
2026-03-29 20:07:51 +0900

    Lv 1 CommerceSystem 클래스 장바구니 내역 출력 기능 추가

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -37,6 +37,13 @@ public class CommerceSystem {
 
             System.out.println("0. 종료 | 프로그램 종료"); // 종료 메뉴 출력
             System.out.println(); // 줄바꿈
+            // 장바구니에 상품이 들어있으면 주문관리 메뉴 출력
+            if (!cart.getCartItemList().isEmpty()) {
+                System.out.println("[ 주문 관리 }");
+                System.out.println("4. 장바구니 확인 | 장바구니를 확인 후 주문합니다.");
+                System.out.println("5. 주문 취소 | 진행중인 주문을 취소합니다.");
+                System.out.println(); // 줄바꿈
+            }
             System.out.print("메뉴 입력: ");
             int categoryChoice = scanner.nextInt(); // 카테고리 메뉴 선택
             System.out.println(); // 줄바꿈
@@ -46,11 +53,11 @@ public class CommerceSystem {
                 System.out.println("커머스 플랫폼을 종료합니다.");
                 break;
             }
+            // 카테고리 번호를 입력한 경우
+            else if (categoryChoice >= 1 && categoryChoice <= categoryList.size()) {
 
-            while (true) {
+                while (true) {
 
-                // 카테고리 번호를 입력한 경우
-                if (categoryChoice >= 1 && categoryChoice <= categoryList.size()) {
                     // 입력한 카테고리 객체를 가져옴
                     Category selectedCategory = categoryList.get(categoryChoice - 1); // 리스트 인덱스는 0부터 시작하기 때문에 1을 빼줌
                     // 선택한 카테고리 출력
@@ -89,6 +96,7 @@ public class CommerceSystem {
                         // 장바구니에 담을지 확인
                         System.out.println("장바구니에 추가하시겠습니가?");
                         System.out.println("1. 확인   2. 취소");
+                        System.out.println(); // 줄바꿈
                         System.out.print("메뉴 입력: ");
                         int cartChoice = scanner.nextInt();
                         System.out.println(); // 줄바꿈
@@ -97,8 +105,9 @@ public class CommerceSystem {
                         if (cartChoice == 1) {
                             CartItem cartItem = new CartItem(selectedProduct, 1);
                             cart.addCartItem(cartItem);
-                            System.out.println("장바구니에 상품이 추가되었습니다.");
+                            System.out.println(selectedProduct.getProductName() + "가 장바구니에 추가되었습니다.");
                             System.out.println(); // 줄바꿈
+                            break;
                         }
                         // 2 입력 시 취소
                         else if (cartChoice == 2) {
@@ -115,10 +124,33 @@ public class CommerceSystem {
                         System.out.println(); // 줄바꿈
                     }
                 }
-                else {
-                    System.out.println("잘못된 카테고리 번호입니다.");
+            }
+            // 4 입력 시 장바구니 확인
+            else if (categoryChoice == 4 && !cart.getCartItemList().isEmpty()) {
+                System.out.println("아래와 같이 주문 하시겠습니까?");
+                System.out.println(); // 줄바꿈
+                // 장바구니에 담긴 상품 목록 출력
+                System.out.println("[ 장바구니 내역 ]");
+                // 장바구니에 담긴 상품들들 하나씩 꺼내서 출력
+
+                for (CartItem cartItem : cart.getCartItemList()) {
+                    // CartItem 안에 들어있는 상품 객체를 가져옴
+                    Product cartProduct = cartItem.getCartProduct();
+                    System.out.println(cartProduct.getProductName() + " | " + String.format("%,d" , cartProduct.getProductPrice()) + "원 | " + cartProduct.getProductDescription() + " | 수량: " + cartItem.getCartQuantity() + "개");
                     System.out.println(); // 줄바꿈
                 }
+                // 총 주문 금액 출력
+                System.out.println("[ 총 주문 금액 ]");
+                System.out.println(String.format("%,d" , cart.calculateTotalPrice()) + "원");
+                System.out.println(); // 줄바꿈
+            }
+            // 5 입력 시 주문 취소
+            else if (categoryChoice == 5 && !cart.getCartItemList().isEmpty()) {
+                System.out.println("진행 중인 주문을 취소합니다.");
+            }
+            else{
+                System.out.println("잘못된 카테고리 번호입니다.");
+                System.out.println(); // 줄바꿈
             }
         }
         scanner.close();
